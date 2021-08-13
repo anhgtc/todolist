2522b5c24ebd21ac399b720d501cdb4a723b4c56
HUST\long.pt173247 <longgtc123@gmail.com>
2021-08-13 16:53:56 +0700

    todolist

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" v-model="newTask">
+    <button @click="tasks.push({content: newTask, done: false})">Add</button>
+    <div v-for="(task,index) in tasks" :key="index">
+      <input type="checkbox" v-model="task.done">
+      <span :class="{done: task.done}">{{task.content}}</span>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      newTask:'',
+      tasks: []
+    }
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.done {
+  text-decoration: line-through;
 }
 </style>
